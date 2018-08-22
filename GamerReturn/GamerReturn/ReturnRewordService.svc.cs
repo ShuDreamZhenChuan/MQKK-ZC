1b328e65174c0ba3ed1270f5c2799dd46d6e4e79
ZhenChuan <kjzhenchuan@sina.com>
2018-08-22 15:08:29 +0800

    master commit

--- GamerReturn/GamerReturn/ReturnRewordService.svc.cs
+++ GamerReturn/GamerReturn/ReturnRewordService.svc.cs
@@ -150,6 +150,8 @@ namespace Moqikaka.GamerReturn
         /// <returns></returns>
         public ReturnMessageBody GetReward(string userId, string partnerId)
         {
+            //Git测试（matser提交）
+
             //构建标准的响应回复数据结构对象
             ReturnMessageBody returnMsg = new ReturnMessageBody();
             try
